7cb2e4ed3a459f175c4b956a6af9fc355c84ab45
RayR <5657448+RayReynolds@users.noreply.github.com>
2022-07-30 23:53:19 +0100

    Update jailPermissions.sh
    notes to follow up

--- Jails/jailPermissions.sh
+++ Jails/jailPermissions.sh
@@ -9,5 +9,18 @@ iocage exec sonarr  service sonarr onestop
 iocage exec sonarr "pw user add media -c media -u 8675309 -d /nonexistent -s /usr/bin/nologin"
 iocage exec sonarr "pw groupadd -n media -g 8675309"
 iocage exec sonarr "pw groupmod media -m sonarr"
-iocage exec sonarr chown -R media:media /usr/local/share/NzbDrone /config
 iocage exec sonarr  sysrc 'sonarr_user=media'
+
+# this can be better, find all files and folders with certain ownership and flip it to media
+iocage exec sonarr chown -R media:media /usr/local/share/NzbDrone /config
+
+# try something like this:
+find / -user tommy -exec chown peter {} \;
+freenas% sudo iocage exec qbittorrent_1 "find / -user qbittorrent"
+
+
+# find background info
+# find will find all files that match a pattern:
+find . -name "*foo"
+# However, if you want a picture:
+tree -P "*foo"
