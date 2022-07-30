30ef34b03c6bfe568329e81418c806f2ba508d03
RayR <5657448+RayReynolds@users.noreply.github.com>
2022-07-30 11:50:24 +0100

    Create jailPermissions.sh

---
+++ Jails/jailPermissions.sh
@@ -0,0 +1,6 @@
+# Interactions between jails seem to have issues with file permissions that cause files to either not be seen or cannot be moved
+# The easiest way around this is to make the media:media user owner of the services running in the jails.
+
+# create media user and group
+# update the service
+# 
