b9cc7cd5059d5e3dfa56f9ede01e3dd70e36a382
RayR <5657448+RayReynolds@users.noreply.github.com>
2022-08-08 18:23:52 +0100

    Update OpenVPNClient.sh

--- Jails/OpenVPNClient.sh
+++ Jails/OpenVPNClient.sh
@@ -1,17 +1,17 @@
 # https://www.reddit.com/r/truenas/comments/o9lvma/best_way_to_run_a_jail_through_vpn/
 
-# create a jail.
-# allow tun traffic... not sure what to do here
+# create a jail (get the commands for this).
+# allow tun traffic
+# UDP 1194 -> 1194
+# TCP 9117 -> 9117
 
+# TrueNAS console
 sudo -i # we need to be root
-iocage console openvpn
-nano /etc/rc.conf
-cat /var/log/openvpn.log
 
+# openvpn config files (additional settings)
+#   auth-user-pass /usr/local/etc/openvpn/login.conf
+#   route 192.168.0.0 255.255.255.0 net_gateway
 
-
-# Steps
-# get openvpn config files (has credential file included)
 cp -R /mnt/Volume_8/Misc/vpn_new/config/ /mnt/Volume_8/iocage/jails/openvpn_create/root/usr/local/etc/openvpn/
 
 iocage console openvpn_create -f
@@ -22,7 +22,7 @@ openvpn --cd /usr/local/etc/openvpn --config uk-lon-st003.prod.surfshark.com_tcp
 
 nano /etc/rc.conf
 # '''
-openvpn_enable=NO #YES
+openvpn_enable=YES #NO #YES
 openvpn_configfile=/usr/local/etc/openvpn/uk-lon-st003.prod.surfshark.com_tcp.ovpn
 openvpn_flags='--ping-restart 240 --verb 4 --log-append /var/log/openvpn.log'
 rtsold_enable="YES"
